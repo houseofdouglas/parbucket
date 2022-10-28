ab6d91a37b12396e74053eec0d943c9583abfab6
Peter Douglas <peter@thehouseofdouglas.com>
2022-10-28 10:17:36 -0600

    Add files via upload

---
+++ src/main/java/com/parbucket/ParBucketApplication.java
@@ -0,0 +1,12 @@
+package com.parbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParBucketApplication.class, args);
+	}
+}
